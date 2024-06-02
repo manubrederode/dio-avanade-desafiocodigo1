6f29f5cb7fb68737348862dd68326ac11ca1fd37
Manu Brederode <brederode.emmanuelle@gmail.com>
2024-06-02 11:24:05 +0100

    Add files via upload

---
+++ DesafioCodigo-1/Program.cs
@@ -0,0 +1,4 @@
+﻿//Desafios de Código Coding The Future Avanade l
+//Módulo Fundamentos do .NET
+//Desafio Codigo 1: Condicionalmente Rico: Tomadas de Decisão no Código
+//Desafio Codigo 2: Lista de itens
